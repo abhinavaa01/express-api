29b2ee935c08999cf7abc989a6ac672459bf79b6
abhinavaa01 <agarwalabhinav91@gmail.com>
2025-04-10 23:33:47 +0530

    todos

--- routes/todos.js
+++ routes/todos.js
@@ -4,38 +4,55 @@ const uuid = require("uuid");
 const fs = require('fs').promises; // Use the promises API for cleaner async/await
 const path = require('path');
 
+router.use(express.json());
+
 const dbFilePath = path.join(__dirname, '../db.json');
 
 router.get("/", async (req, res) => {
-    if (Object.keys(req.query).length) console.log(req.query);
+    const { userEmail, id, isCompleted } = req.query;
     try {
         const data = await fs.readFile(dbFilePath, 'utf8');
         const jsonFile = JSON.parse(data);
-        res.json(jsonFile.todos);
+        let todos = jsonFile.todos;
+        if (id) {
+            todos = todos.filter((todo)=> todo.id === id);
+        }
+        if (userEmail) {
+            todos = todos.filter((todo)=> todo.userEmail === userEmail);
+        }
+        if (isCompleted) {
+            todos = todos.filter((todo)=> todo.isCompleted === isCompleted);
+        }
+
+        res.json(todos);
     } catch (error) {
         console.error('Error reading db.json:', error);
         res.status(500).send('Error reading data');
     }
 });
 
-router.route("/").post(async (req, res) => {
+router.post("/", async (req, res) => {
+    // console.log(req.body.text);
+    // res.send("hello");
         try {
-            const { text } = req.body;
+            const { text, image = "", id = uuid.v4(), userEmail="abhinav@bahikhata.org" } = req.body;
             if (!text) {
                 return res.status(400).send('Please provide the text for the new todo.');
             }
 
             const newTodo = {
-                id: uuid.v4(),
+                id: id,
+                userEmail: userEmail,
                 text: text,
-                completed: false,
+                image: image,
+                isCompleted: false
             };
 
             const data = await fs.readFile(dbFilePath, 'utf8');
             const jsonFile = JSON.parse(data);
             jsonFile.todos.push(newTodo);
 
-            await fs.writeFile(dbFilePath, JSON.stringify(jsonFile, null, 2), 'utf8');
+            fs.writeFile(dbFilePath, JSON.stringify(jsonFile, null, 2), 'utf8');
 
             res.status(201).json(newTodo);
         } catch (error) {
@@ -44,8 +61,37 @@ router.route("/").post(async (req, res) => {
         }
     });
 
-router.route("/:id").get((req, res) => {
-        res.send("User with id: " + req.params.id);
-    });
+router.get("/:id", (async(req, res) => {
+        const data = await fs.readFile(dbFilePath, 'utf8');
+        const jsonFile = JSON.parse(data);
+        const todos = jsonFile.todos;
+        const result = todos.find((todo)=>todo.id === req.params.id)
+        if (result) {
+            res.json(result);
+        } else {
+            res.status(404).send("Not Found");
+        }
+    }));
+
+router.delete("/:id", (async(req, res) => {
+    try {
+        const data = await fs.readFile(dbFilePath, 'utf8');
+        const jsonFile = JSON.parse(data);
+        const todos = jsonFile.todos;
+    
+        const index = todos.findIndex(todo => todo.id === req.params.id);
+    
+        if (index !== -1) {
+          const deleted = todos.splice(index, 1)[0]; 
+          await fs.writeFile(dbFilePath, JSON.stringify(jsonFile, null, 2), 'utf8');
+          res.json({ message: "Todo deleted", todo: deleted });
+        } else {
+          res.status(404).send("Todo not found");
+        }
+      } catch (error) {
+        console.error("Error deleting todo:", error);
+        res.status(500).send("Error deleting todo");
+      }
+}));
 
 module.exports = router;
